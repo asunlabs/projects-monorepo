2d528593954406abc46fc4328dd3a0a489969a2f
designerasun <asuntheawesome@gmail.com>
2022-07-20 01:21:20 +0900

    ENH:add base mint amount, ropsten USDC

--- apps/smart-contracts/contracts/tokenSwap/erc20Swap/Dex.sol
+++ apps/smart-contracts/contracts/tokenSwap/erc20Swap/Dex.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.6.6;
-// pragma solidity ^0.8.15;
 
 import "@uniswap/v2-periphery/contracts/UniswapV2Router02.sol";
 
@@ -9,6 +8,7 @@ contract Dex {
     address internal constant UNISWAP_V2_ROUTER = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
     IUniswapV2Router02 public uniswapRouter;
     address internal constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+    address internal constant USDC_ROPSTEN = 0x07865c6E87B9F70255377e024ace6630C1Eaa37F;
 
     constructor() public payable {
         uniswapRouter = IUniswapV2Router02(UNISWAP_V2_ROUTER);
@@ -22,26 +22,29 @@ contract Dex {
         uniswapRouter.swapExactETHForTokens{value: msg.value}(amountOutMin, path, msg.sender, deadline);
     }
 
-    function swapUSDCForEth(uint256 tokenAmount) external payable {
+    // FIX: delete payable, change USDC ropsten address
+    function swapUSDCForEth(uint256 tokenAmount) external {
         uint256 deadline = block.timestamp + 150;
         address[] memory path = getUSDCForEthPath();
         uint256 amountOutMin = uniswapRouter.getAmountsOut(tokenAmount, path)[1];
-        IERC20(USDC).transferFrom(msg.sender, address(this), tokenAmount);
-        IERC20(USDC).approve(UNISWAP_V2_ROUTER, tokenAmount);
+        // IERC20(USDC).transferFrom(msg.sender, address(this), tokenAmount);
+        // IERC20(USDC).approve(UNISWAP_V2_ROUTER, tokenAmount);
+        IERC20(USDC_ROPSTEN).transferFrom(msg.sender, address(this), tokenAmount);
+        IERC20(USDC_ROPSTEN).approve(UNISWAP_V2_ROUTER, tokenAmount);
         uniswapRouter.swapExactTokensForETH(tokenAmount, amountOutMin, path, msg.sender, deadline);
     }
 
     function getEthForUSDCPath() private view returns (address[] memory) {
         address[] memory path = new address[](2);
         path[0] = uniswapRouter.WETH();
-        path[1] = USDC;
+        path[1] = USDC_ROPSTEN;
 
         return path;
     }
 
     function getUSDCForEthPath() private view returns (address[] memory) {
         address[] memory path = new address[](2);
-        path[0] = USDC;
+        path[0] = USDC_ROPSTEN;
         path[1] = uniswapRouter.WETH();
 
         return path;
---
+++ apps/smart-contracts/contracts/tokenSwap/erc20Swap/MockERC20.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.8.15;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract ERC20Mock is ERC20 {
+    constructor() public ERC20("USDC Coin", "USDC") {
+        _mint(msg.sender, 1000 * 10**decimals());
+    }
+}
