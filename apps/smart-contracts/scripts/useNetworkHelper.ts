61c76bb5d347eadf2543177a69d117345f487f09
designerasun <asuntheawesome@gmail.com>
2022-07-22 00:45:51 +0900

    ENH:add impersonated signer with network helper

--- apps/smart-contracts/scripts/useNetworkHelper.ts
+++ apps/smart-contracts/scripts/useNetworkHelper.ts
@@ -1,19 +1,30 @@
 import * as helpers from "@nomicfoundation/hardhat-network-helpers";
 import { ethers } from "hardhat";
+import hre from "hardhat";
+
+const scriptCommand = "pnpm exec hardhat run scripts/useNetworkHelper.ts";
 
 async function main() {
-  // mine 100 blocks
-  await helpers.mine(100);
-  console.log("The current block number is", await helpers.time.latestBlock());
-  const address = "0x1B7BAa734C00298b9429b518D621753Bb0f6efF2";
-  await helpers.impersonateAccount(address);
-  // console.log(ethers.provider.getSigner(address));
+  const DAI_WHALE_MAINNET = "0x1B7BAa734C00298b9429b518D621753Bb0f6efF2";
+  await helpers.impersonateAccount(DAI_WHALE_MAINNET);
 
-  const impersonatedSigner = await ethers.getSigner(address);
-  console.log(impersonatedSigner);
-  // now you can send a transaction
-  // impersonatedSigner.sendTransaction()
+  const impersonatedSigner = await impersonateAddress(DAI_WHALE_MAINNET);
+  const [recipient] = await ethers.getSigners();
+  const response = await impersonatedSigner.sendTransaction({
+    to: recipient.address,
+    value: ethers.utils.parseUnits("1", "ether"),
+  });
+
+  console.log("tx hash: ", response.hash);
+  console.log(await recipient.getBalance());
 }
+
+// convert address to impersonated signer
+const impersonateAddress = async (address: string) => {
+  await helpers.impersonateAccount(address);
+  const impersonateSigner = ethers.getSigner(address);
+  return impersonateSigner;
+};
 
 main().catch((error) => {
   console.error(error);
