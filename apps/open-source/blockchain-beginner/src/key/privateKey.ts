ed9bd7c08565aebc1b3bcac6cec1a435a626c0be
designerasun <asuntheawesome@gmail.com>
2022-07-24 00:38:30 +0900

    ENH:add public key gen, facade design pattern

--- apps/open-source/blockchain-beginner/src/key/privateKey.ts
+++ apps/open-source/blockchain-beginner/src/key/privateKey.ts
@@ -1,9 +1,13 @@
 import CryptoJS from "crypto-js";
 import { v4 } from "uuid";
 
+const PATH = "../../node_modules/.bin/ts-node privateKey";
+
 const privateKeys = new Map(); // key: username, value: private key
+const publicKeys = new Map();
 const users = new Map(); // key: username, value: user id
 
+// Mimic: Redux action creator => Reducer(switch~case)
 function enrollUser(name: string) {
   return {
     username: name,
@@ -12,29 +16,60 @@ function enrollUser(name: string) {
   };
 }
 
-console.log(CryptoJS.format.Hex.parse(generatePrivateKey()));
+const seed = "developerasun";
 
-function generatePrivateKey() {
-  const seed = "cool cat";
+function generatePrivateKey(seed: string) {
   // dynamic key generation
   return CryptoJS.SHA256(seed.concat(v4())).toString();
 }
 
-function initSignUp(data: Map<any, any>, key: string, callback: Function) {
-  data.set(key, callback(key));
+function generatePublicKey(seed: string) {
+  const privateKey = generatePrivateKey(seed);
+  // ECDSA: Elliptic Curve Digital Signature Algorithm
+  return CryptoJS.SHA256(privateKey.concat(seed)).toString();
 }
 
-initSignUp(users, "Jake", enrollUser);
-initSignUp(users, "Sally", enrollUser);
-initSignUp(privateKeys, "Jake", generatePrivateKey);
-initSignUp(privateKeys, "Sally", generatePrivateKey);
+function initSignUp(data: Map<any, any>, key: string, callback: Function) {
+  if (!callback.name.includes("generate")) {
+    data.set(key, callback(key));
+  } else {
+    data.set(key, callback(seed));
+  }
+}
 
-// console.log("%cprivateKey.js line:31 users", "color: #007acc;", users);
-// console.log(
-//   "%cprivateKey.js line:32 privateKeys",
-//   "color: #007acc;",
-//   privateKeys
-// );
+function initWithoutPattern() {
+  initSignUp(users, "Jake", enrollUser);
+  initSignUp(users, "Sally", enrollUser);
+  initSignUp(privateKeys, "Jake", generatePrivateKey);
+  initSignUp(privateKeys, "Sally", generatePrivateKey);
+  initSignUp(publicKeys, "Jake", generatePublicKey);
+  initSignUp(publicKeys, "Sally", generatePublicKey);
+}
 
-console.log(generatePrivateKey().length); // hex 64
-export { privateKeys, users };
+class initWithPattern {
+  static SEED = "developerasun";
+  name;
+  constructor(_name: string) {
+    this.name = _name;
+  }
+  enroll() {
+    enrollUser(this.name);
+    this.initPrivateKey();
+    this.initPublicKey();
+    return {
+      privateKeys,
+      publicKeys,
+      users,
+    };
+  }
+  private initPrivateKey() {
+    generatePrivateKey(initWithPattern.SEED);
+  }
+  private initPublicKey() {
+    generatePublicKey(initWithPattern.SEED);
+  }
+}
+
+initWithoutPattern();
+
+export { privateKeys, users, publicKeys, initWithPattern };
