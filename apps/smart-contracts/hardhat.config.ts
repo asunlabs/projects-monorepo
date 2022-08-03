3d7a06f62fab25dcf21c1519cd7c0b39947a8709
designerasun <asuntheawesome@gmail.com>
2022-08-03 23:45:35 +0900

    BLD:update configs

--- apps/smart-contracts/hardhat.config.ts
+++ apps/smart-contracts/hardhat.config.ts
@@ -9,23 +9,26 @@ import "@openzeppelin/hardhat-upgrades";
 import "@nomiclabs/hardhat-ethers";
 import "@nomicfoundation/hardhat-chai-matchers";
 import { PLUGIN } from "./scripts/manager/constantManager";
-import { requireElement } from "./scripts/hooks/useConfigError";
 
 dotenv.config({ path: "./.env.development" });
 
 const {
+  TEST_GOERLI_URL,
+  TEST_SEPOLIA_URL,
   TEST_ROPSTEN_URL,
   TEST_RINKEBY_URL,
   TEST_KOVAN_URL,
-  TEST_GOERLI_URL,
 
   MAIN_ETHEREUM_URL,
   FORK_MAINNET_URL,
+  FORK_GOERLI_URL,
 
+  ACCOUNT_ETHEREUM_PRIVATE_KEY,
+  ACCOUNT_GOERLI_PRIVATE_KEY,
+  ACCOUNT_SEPOLIA_PRIVATE_KEY,
   ACCOUNT_ROPSTEN_PRIVATE_KEY,
   ACCOUNT_RINKEBY_PRIVATE_KEY,
   ACCOUNT_KOVAN_PRIVATE_KEY,
-  ACCOUNT_ETHEREUM_PRIVATE_KEY,
 
   ACCOUNT_DEPLOYER_PRIVATE_KEY,
   ACCOUNT_DEPLOYER_ADDRESS,
@@ -82,11 +85,22 @@ const config: HardhatUserConfig = {
         enabled: true,
       },
     },
-    // 20220803 TO DO: goerli should be added
     goerli: {
-      url: TEST_GOERLI_URL !== undefined ? TEST_GOERLI_URL : "",
-      accounts: [],
+      url: TEST_GOERLI_URL !== undefined ? String(TEST_GOERLI_URL) : "",
+      accounts:
+        ACCOUNT_GOERLI_PRIVATE_KEY !== undefined
+          ? [ACCOUNT_GOERLI_PRIVATE_KEY]
+          : [],
     },
+    // node provider not yet actively supports sepolia. currently url is empty string.
+    sepolia: {
+      url: TEST_SEPOLIA_URL !== undefined ? TEST_SEPOLIA_URL : "",
+      accounts:
+        ACCOUNT_SEPOLIA_PRIVATE_KEY !== undefined
+          ? [ACCOUNT_SEPOLIA_PRIVATE_KEY]
+          : [],
+    },
+    // @deprecated
     ropsten: {
       url: TEST_ROPSTEN_URL ? String(TEST_ROPSTEN_URL) : "",
       accounts:
@@ -94,6 +108,7 @@ const config: HardhatUserConfig = {
           ? [ACCOUNT_ROPSTEN_PRIVATE_KEY]
           : [],
     },
+    // @deprecated
     rinkeby: {
       url: TEST_RINKEBY_URL ? String(TEST_RINKEBY_URL) : "",
       accounts:
@@ -101,6 +116,7 @@ const config: HardhatUserConfig = {
           ? [ACCOUNT_RINKEBY_PRIVATE_KEY]
           : [],
     },
+    // @deprecated
     kovan: {
       url: TEST_KOVAN_URL ? String(TEST_KOVAN_URL) : "",
       accounts:
@@ -124,6 +140,7 @@ const config: HardhatUserConfig = {
   },
   etherscan: {
     apiKey: {
+      goerli: API_ETHERSCAN_KEY !== undefined ? API_ETHERSCAN_KEY : "",
       ropsten: API_ETHERSCAN_KEY !== undefined ? API_ETHERSCAN_KEY : "",
       rinkeby:
         API_ETHERSCAN_BACKUP_KEY !== undefined ? API_ETHERSCAN_BACKUP_KEY : "",
