57d1597365c7e6d0ef29a14e65bebe2a9d40f6b7
Jake Sung <asuntheawesome@gmail.com>
2022-08-04 16:12:54 +0900

    ENH:ERC777 token holder - operator explained

--- apps/smart-contracts/contracts/token/non-upgrade/ERC777/MyFirstERC777.sol
+++ apps/smart-contracts/contracts/token/non-upgrade/ERC777/MyFirstERC777.sol
@@ -1,9 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.15;
 
+/// @dev ERC777 is not deployable in Remix IDE
 import "@openzeppelin/contracts/token/ERC777/ERC777.sol";
 contract MyFirstERC777 is ERC777 {
-    constructor() ERC777("MyFirst777", "MF", new address[](0)){}
+    /**
+     * receive hook: prevent user error (revert if callee contract not implementing token interface)
+     * decimals in ERC777 is fixed as 18
+     * operator and token holder: operator can execute token transfer if authorized
+     * operator can use: operatorSend, operatorBurn
+     * operator access control is: authroizeOperator, isOperatorFor, revokeOperator
+     * operator must have enough tokens and ether for transaction execution
+     */
+    constructor(uint256 initialSupply, address[] memory defaultOperator) ERC777("MyFirst777", "MF", defaultOperator){
+        _mint(msg.sender, initialSupply * uint256(10 ** decimals()), "", "");
+    }
+    function mint(address _account, uint256 _amount, bytes memory _userData, bytes memory _operatorData) public {
+        _mint(_account, _amount, _userData, _operatorData);
+    }
 
     function getInterfaceId() external pure returns(bytes4) {
         return type(IERC777).interfaceId;
