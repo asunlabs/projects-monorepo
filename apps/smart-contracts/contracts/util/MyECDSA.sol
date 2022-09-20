e18498d22a114548b01eef49623e01c4b0fc20df
designerasun <asuntheawesome@gmail.com>
2022-09-21 00:06:15 +0900

    ENH:verify signature with ECDSA and ethersjs

--- apps/smart-contracts/contracts/util/MyECDSA.sol
+++ apps/smart-contracts/contracts/util/MyECDSA.sol
@@ -6,25 +6,19 @@ import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 contract MyECDSA {
     using ECDSA for bytes32;
 
-    /// @dev a common type of signature is a bytes65 array, with v(1), r(32), s(32) elements. 
-    struct RawSignature {
-        bytes8 v;
-        bytes32 r;
-        bytes32 s;
-    }
-    
-    function hashData(string memory _data) public pure returns(bytes32) {
-        bytes32 data = keccak256(abi.encode(_data));
-        return data;
-    }
-
     /**
-     * recover: returns an address and error enum. this should be the same address
-     * that has sent a transaction and signed the message.
+     * * Verify if a recovered address is the same with the message signer.
+     * get signature with ethers js, for example, 
+     const hashedMessage = ethers.utilss.hashMessage('jake') // hashedData
+     const signature = ethers.utils.signMessage(hashedMessage)
      */
-    function verify(bytes32 data, bytes memory signature, address account) public pure returns(bool) {
-        return data
-            .toEthSignedMessageHash() // prefix: '\x19Ethereum Signed Message:\n'
-            .recover(signature) == account; 
+    function verify(
+        bytes32 hashedData,
+        bytes memory signature,
+        address messageSigner
+    ) public pure returns (bool) {
+        bytes32 signedMessage = hashedData.toEthSignedMessageHash();
+        address recoveredAddress = signedMessage.recover(signature);
+        return recoveredAddress == messageSigner ? true : false;
     }
 }
