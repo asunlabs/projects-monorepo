e7ac87c7996bf51eee6b16cd8118ed33997fad16
designerasun <asuntheawesome@gmail.com>
2022-10-01 18:00:52 +0900

    CLN:fix abi getter hook

--- apps/smart-contracts/scripts/hooks/useArtifacts.ts
+++
@@ -1,6 +0,0 @@
-import hre from "hardhat";
-
-export async function useABIgetter(contractName: string) {
-  const contract = await hre.artifacts.readArtifact(contractName);
-  return { contract };
-}
--- apps/smart-contracts/scripts/hooks/useNetworkHelper.ts
+++ apps/smart-contracts/scripts/hooks/useNetworkHelper.ts
@@ -1,5 +1,12 @@
 import { impersonateAccount, takeSnapshot } from "@nomicfoundation/hardhat-network-helpers";
 import { ethers } from "hardhat";
+import hre from "hardhat";
+
+export async function useABIgetter(contractName: string) {
+  const { abi, bytecode, deployedBytecode } = await hre.artifacts.readArtifact(contractName);
+
+  return { abi, bytecode, deployedBytecode };
+}
 
 export async function useImpersonatedSigner(address: string) {
   await impersonateAccount(address);
