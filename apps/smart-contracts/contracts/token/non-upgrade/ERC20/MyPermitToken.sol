de2ed3be5fe8c1aeca51ada342740d93e40ff97b
designerasun <asuntheawesome@gmail.com>
2022-10-03 23:27:33 +0900

    ENH:add ERC20Permit to replace ERC20 approve

---
+++ apps/smart-contracts/contracts/token/non-upgrade/ERC20/MyPermitToken.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.17;
+
+import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
+import "hardhat/console.sol";
+
+contract MyPermitToken is ERC20, ERC20Permit {
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) ERC20Permit(name) {}
+
+    function mint(address to, uint256 amount) external {
+        _mint(to, amount);
+    }
+}
