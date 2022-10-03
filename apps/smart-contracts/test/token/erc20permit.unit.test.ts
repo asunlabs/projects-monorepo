f5c81e11f57520c92b60f6e3cf490c2e5983da33
designerasun <asuntheawesome@gmail.com>
2022-10-03 23:26:45 +0900

    TST:add ERC20Permit test cases

---
+++ apps/smart-contracts/test/token/erc20permit.unit.test.ts
@@ -0,0 +1,95 @@
+import { ethers } from "hardhat";
+import { BigNumber, Contract } from "ethers";
+import { expect } from "chai";
+import { loadFixture, time } from "@nomicfoundation/hardhat-network-helpers";
+import chalk from "chalk";
+
+import dotenv from "dotenv";
+
+dotenv.config({ path: "../../.env.development" });
+
+const { ACCOUNT_GOERLI_PRIVATE_KEY } = process.env;
+
+const PREFIX = "unit-MyPermit";
+let vault: Contract;
+let erc20Token: Contract;
+
+// @dev hardhat's new fixture
+const useFixture = async () => {
+  const MyPermitTokenFactory = await ethers.getContractFactory("MyPermitToken");
+  const _myPermitToken = await MyPermitTokenFactory.deploy("jakeToke", "jake");
+  const ERC20Token = await _myPermitToken.deployed();
+  erc20Token = ERC20Token;
+
+  const MyPermit = await ethers.getContractFactory("MyPermitVault");
+  const myPermit = await MyPermit.deploy(ERC20Token.address);
+  vault = await myPermit.deployed();
+
+  const [owner, recipient] = await ethers.getSigners();
+
+  return { vault, erc20Token, owner, recipient };
+};
+
+describe(`${PREFIX}-ERC20Permit/EIP712`, async function TestPermission() {
+  it.only("Should permit for a signer", async function TestERC20Permit() {
+    const { vault, erc20Token, owner, recipient } = await loadFixture(useFixture);
+
+    // useERC20Permit.ts => setDeadline, getDeadline
+    const oneMinute = time.duration.minutes(1);
+    const currentTimestamp = await time.latest();
+    const deadline = currentTimestamp + oneMinute;
+
+    const provider = ethers.getDefaultProvider("goerli");
+    const signer = new ethers.Wallet(ACCOUNT_GOERLI_PRIVATE_KEY!, provider);
+
+    const mintAmount = ethers.utils.parseEther("1");
+    await erc20Token.mint(signer.address, mintAmount);
+    // sign message
+    // await signer.// split signature
+    // permitted deposit to contract
+    /**
+     *  IERC20 token,
+        uint256 deadline,
+        uint256 amount,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+     */
+    const domainData = {
+      name: "MyPermitVault",
+      version: "1",
+      chainId: await owner.getChainId(),
+      verifyingContract: vault.address,
+    };
+
+    // ERC20Permit typeHash: Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline
+    const types = {
+      Permit: [
+        { name: "owner", type: "address" },
+        { name: "spender", type: "address" },
+        { name: "value", type: "uint256" },
+        { name: "nonce", type: "uint256" },
+        { name: "deadline", type: "uint256" },
+      ],
+    };
+
+    // data to sign
+    const value = {
+      owner: signer.address, // token holder
+      spender: vault.address,
+      value: mintAmount,
+      nonce: await signer.provider.getTransactionCount(signer.address),
+      deadline: deadline,
+    };
+
+    const signature = await signer._signTypedData(domainData, types, value);
+    console.log({ signature });
+    const { v, r, s } = ethers.utils.splitSignature(signature);
+
+    // TODO fix token balance not moved error
+    await vault.connect(signer).verifyPermissionAndTransfer(mintAmount, deadline, v, r, s);
+    console.log("signer token bal: ", BigInt(await erc20Token.balanceOf(signer.address)));
+
+    console.log(await erc20Token.balanceOf(vault.address));
+  });
+});
